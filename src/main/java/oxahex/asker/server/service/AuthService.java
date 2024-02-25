f6e267407bdf44566e59cb511b245597d6ec46bc
oxahex <oxahex@gmail.com>
2024-02-25 13:45:48 +0900

    feat: UserDetailsService 생성

---
+++ src/main/java/oxahex/asker/server/service/AuthService.java
@@ -0,0 +1,19 @@
+package oxahex.asker.server.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+}
