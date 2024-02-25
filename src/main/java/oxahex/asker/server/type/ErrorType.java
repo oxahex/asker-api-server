581cd600450737f95bab6cd8a6764c3c801d18d2
oxahex <oxahex@gmail.com>
2024-02-25 22:03:48 +0900

    feat: 이메일 회원가입 기능 구현
    - 이메일 중복 검증
    - 이메일 중복 시 CONFLICT 반환

--- src/main/java/oxahex/asker/server/type/ErrorType.java
+++ src/main/java/oxahex/asker/server/type/ErrorType.java
@@ -11,7 +11,10 @@ public enum ErrorType {
 	// 인증/인가
 	AUTHENTICATION_FAILURE(HttpStatus.UNAUTHORIZED, "인증 정보가 없거나 올바르지 않습니다."),
 	AUTHORIZATION_FAILURE(HttpStatus.FORBIDDEN, "권한이 없습니다."),
-	TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 토큰입니다.");
+	TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 토큰입니다."),
+
+	// 중복
+	EMAIL_CONFLICT(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String errorMessage;
