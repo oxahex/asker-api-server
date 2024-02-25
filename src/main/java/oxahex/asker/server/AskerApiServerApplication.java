0e8ae64fa4e0a0fb9dac701f51e41f50f69a51f7
oxahex <oxahex@gmail.com>
2024-02-25 12:00:20 +0900

    init project

---
+++ src/main/java/oxahex/asker/server/AskerApiServerApplication.java
@@ -0,0 +1,13 @@
+package oxahex.asker.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskerApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskerApiServerApplication.class, args);
+	}
+
+}
